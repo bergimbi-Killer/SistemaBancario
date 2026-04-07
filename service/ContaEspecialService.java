6377546b828eb0171fb3a9fbb7c8a69d43df403c
Bernardo Gimbi <bergimbirgmail.com>
2026-04-07 13:59:15 +0100

    Injeção de dependencias, feito o construtor e o metodo criarConta().

---
+++ service/ContaEspecialService.java
@@ -0,0 +1,29 @@
+
+package service;
+
+import domain.exception.ClienteNaoEncontradoExcenption;
+import domain.model.Cliente;
+import domain.model.Conta;
+import domain.model.ContaEspecial;
+import repository.ClienteRepository;
+import repository.ContaEspecialRepository;
+
+public class ContaEspecialService {
+    
+    private final ContaEspecialRepository contaEspecialRepository;
+    private final ClienteRepository clienteRepository;
+    
+    public ContaEspecialService (ContaEspecialRepository contaEspecialRepository, ClienteRepository ClienteRepository) {
+        this.contaEspecialRepository = contaEspecialRepository;
+        this.clienteRepository = ClienteRepository;
+    }
+    
+    public Conta criarConta(Cliente titular, double saldo, double percentagemDeAumento, String biCliente) throws ClienteNaoEncontradoExcenption {
+        int id = contaEspecialRepository.gerarNovoID();
+        Cliente cliente = clienteRepository.buscarPorBI(biCliente);
+        Conta conta = new ContaEspecial (id,saldo,cliente,percentagemDeAumento);
+        contaEspecialRepository.salvar(conta);
+        return conta;
+    }
+}
+
